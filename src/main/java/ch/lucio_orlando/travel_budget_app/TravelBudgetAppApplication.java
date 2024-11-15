cc6a7ec297370129789fcef865fbc9e46d731695
Lucio Orlando <lucio01.orlando@gmail.com>
2024-11-15 16:48:17 +0100

    develop: initial project set-up

---
+++ src/main/java/ch/lucio_orlando/travel_budget_app/TravelBudgetAppApplication.java
@@ -0,0 +1,13 @@
+package ch.lucio_orlando.travel_budget_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBudgetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBudgetAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/lucio_orlando/travel_budget_app/TravelBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.lucio_orlando.travel_budget_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
