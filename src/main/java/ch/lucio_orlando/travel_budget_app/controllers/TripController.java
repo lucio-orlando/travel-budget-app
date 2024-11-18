f81ecfeab569f2526225cdea189cde4d37525e08
Lucio Orlando <lucio01.orlando@gmail.com>
2024-11-18 10:32:56 +0100

    develop: adjust trip mapping and function names

--- src/main/java/ch/lucio_orlando/travel_budget_app/controllers/TripController.java
+++ src/main/java/ch/lucio_orlando/travel_budget_app/controllers/TripController.java
@@ -11,31 +11,31 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Controller
-@RequestMapping("/trips")
+@RequestMapping("/trip")
 public class TripController {
 
     @Autowired
     private TripService tripService;
 
     @GetMapping
-    public String getTrips(Model model) {
+    public String overview(Model model) {
         List<Trip> trips = tripService.getTrips();
         model.addAttribute("trips", trips);
         return "trip/list";
     }
 
     @GetMapping({"/new", "/edit/{id}"})
-    public String showTripForm(@PathVariable(required = false) Long id, Model model) {
+    public String form(@PathVariable(required = false) Long id, Model model) {
         Trip trip = (id != null) ? tripService.getTripById(id).orElse(null) : new Trip();
 
-        if (trip == null) return "redirect:/trips";
+        if (trip == null) return "redirect:/trip";
 
         model.addAttribute("trip", trip);
         return "trip/create-edit";
     }
 
     @PostMapping
-    public String saveTrip(@ModelAttribute Trip trip, @RequestParam String startDate, @RequestParam(required = false) String endDate) {
+    public String save(@ModelAttribute Trip trip, @RequestParam String startDate, @RequestParam(required = false) String endDate) {
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             trip.setStartDate(dateFormat.parse(startDate));
@@ -45,15 +45,15 @@ public class TripController {
             }
 
             tripService.saveTrip(trip);
-            return "redirect:/trips";
+            return "redirect:/trip";
         } catch (Exception e) {
             return "error";
         }
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteTrip(@PathVariable Long id) {
+    public String delete(@PathVariable Long id) {
         tripService.deleteTrip(id);
-        return "redirect:/trips";
+        return "redirect:/trip";
     }
 }
