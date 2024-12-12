8877c8ff7de1352d8444e3fff4a4c7e28ce82c4b
Lucio Orlando <lucio01.orlando@gmail.com>
2024-12-12 16:23:08 +0100

    develop: add subTrip to demo-data

--- src/main/java/ch/lucio_orlando/travel_budget_app/loader/DemoDataLoader.java
+++ src/main/java/ch/lucio_orlando/travel_budget_app/loader/DemoDataLoader.java
@@ -27,7 +27,11 @@ public class DemoDataLoader {
 
     private void loadDemoTrips() {
         if (tripRepository.count() == 0) {
-            tripRepository.save(getAsiaDemoTrip());
+            Trip asiaTrip = getAsiaDemoTrip();
+            Trip vietnamTrip = getVietnamDemoTrip();
+            asiaTrip.addComponent(vietnamTrip);
+            tripRepository.save(asiaTrip);
+            tripRepository.save(vietnamTrip);
             tripRepository.save(getCentralAmericaDemoTrip());
         }
     }
@@ -38,6 +42,15 @@ public class DemoDataLoader {
         trip.setDate(new Date());
         trip.setEndDate(new Date(System.currentTimeMillis() + 14L * 24 * 60 * 60 * 1000));
         trip.setImage("https://images.unsplash.com/photo-1469487885741-33b975dd5bbc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NzgyNTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MzM3NTE2ODd8&ixlib=rb-4.0.3&q=80&w=1080");
+        return trip;
+    }
+
+    private Trip getVietnamDemoTrip() {
+        Trip trip = new Trip();
+        trip.setName("Vietnam");
+        trip.setDate(new Date());
+        trip.setEndDate(new Date(System.currentTimeMillis() + 14L * 24 * 60 * 60 * 1000));
+        trip.setImage("https://images.unsplash.com/photo-1578039821447-e3884b8472a0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NzgyNTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MzQwMTY3ODJ8&ixlib=rb-4.0.3&q=80&w=1080");
         return trip;
     }
 
